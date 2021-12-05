e1a6a64ef4347f9948641a86b54408bbfaec1747
Redx <78570858+Unique-Red@users.noreply.github.com>
2021-12-05 18:47:40 +0100

    Add files via upload

---
+++ myshop/shop/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_manager
+import os
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "helloworld"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myshop.db'
+
+db = SQLAlchemy(app)
+
+
+from shop.admin.models import User
+
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+from .admin import routes
+from .products import routes
