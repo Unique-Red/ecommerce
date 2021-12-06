1d661e1517670e07a5151b2dbf3fcd2d5edfe34b
Redx <78570858+Unique-Red@users.noreply.github.com>
2021-12-06 23:57:03 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,27 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-#from flask.ext.uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
-from werkzeug.utils import secure_filename
-from werkzeug.datastructures import  FileStorage
-
-from flask_uploads import IMAGES, UploadSet, configure_uploads, patch_request_class
+from flask_login import LoginManager, login_manager
 import os
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "helloworld"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myshop.db'
-app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'static/images')
-
-photos = UploadSet('photos', IMAGES)
-configure_uploads(app, photos)
-patch_request_class(app)
 
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
 
+
+from shop.admin.models import User
+
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 from .admin import routes
 from .products import routes
+from .carts import carts
